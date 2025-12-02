86a825e4d2bcc527adc499d0c139b270d8f6c4a3
kiarash-naderi <kiarashnaaderi@gmail.com>
2025-12-02 13:27:46 +0330

    add basic Limitter

---
+++ src/api/middleware/test_middleware.go
@@ -0,0 +1,20 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+
+func TestMiddleware() gin.HandlerFunc  {
+	return func(ctx *gin.Context) {
+		apikey := ctx.GetHeader("x-api-key")
+		if apikey == "1"{
+			ctx.Next()
+		}
+		ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
+			"result": "Api key is required",
+		})
+	}
+}
